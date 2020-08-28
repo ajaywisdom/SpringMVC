e6a96289e83ab62c1aa1c3514104c48ff8ec742a
ajaywisdom <ajay@wisdomtreeventures.com>
2020-08-29 00:49:29 +0530

    Sending Form Data to Controller And Handle By @ModelAttribute

--- src/main/java/springmvc/controller/Contact.java
+++ src/main/java/springmvc/controller/Contact.java
@@ -3,9 +3,13 @@ package springmvc.controller;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import springmvc.model.ContactModel;
 
 @Controller	
 @RequestMapping("/contact")
@@ -14,9 +18,15 @@ public class Contact {
 	public String show() {
 		return "contact";
 	}
+//	@RequestMapping(path="/create", method=RequestMethod.POST)
+//	public String create(@RequestParam("email") String email, @RequestParam("password") String pass) {
+//		System.out.println(email+" "+pass);
+//		return "null";
+//	}
+	
 	@RequestMapping(path="/create", method=RequestMethod.POST)
-	public String create(@RequestParam("email") String email, @RequestParam("password") String pass) {
-		System.out.println(email+" "+pass);
-		return "null";
+	public String create(@ModelAttribute ContactModel contact, Model model) {
+		model.addAttribute("email",contact.getEmail());
+		return "success";
 	}
 }
